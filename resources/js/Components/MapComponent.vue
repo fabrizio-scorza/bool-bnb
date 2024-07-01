a5a7db5366dd999455cc2b1130c59b3b0ca40752
fabrizio-scorza <fabrizio.a.scorza@gmail.com>
2024-07-01 10:01:40 +0200

    FIX map component

--- resources/js/Components/MapComponent.vue
+++ resources/js/Components/MapComponent.vue
@@ -1,36 +1,47 @@
 <script>
-// import tt from "@tomtom-international/web-sdk-maps"
+import tt from "@tomtom-international/web-sdk-maps"
 
-// export default {
-//     data() {
-//         return {
-//             map: null
-//         }
-//     },
+export default {
+    data() {
+        return {
+        }
+    },
 
-//     mounted() {
-//         this.initMap();
-//     },
-//     methods: {
-//         initMap() {
-//             let center = [14, 42.4]
-//             this.map = tt.map({
-//                 key: "0GaJeIIm0LI9Q7jst1IQIUzSO9jDa8m6",
-//                 container: "map",
-//                 zoom: 10
-//             })
+    mounted() {
+        this.loadMap();
+    },
+    methods: {
+        loadMap() {
+            const latitude = 41.87;
+            const longitude = 12.47;
 
-//             this.map.on('load', () => {
-//                 new tt.Marker().setLngLat(center).addTo(this.map)
-//             })
-//         }
-//     }
-// }
+            const map = tt.map({
+                key: "0GaJeIIm0LI9Q7jst1IQIUzSO9jDa8m6",
+                container: "map",
+                center: [longitude, latitude],
+                zoom: 12
+            })
+            const marker = new tt.Marker().setLngLat([longitude, latitude]).addTo(map)
+
+            map.addControl(new tt.NavigationControl(), 'top-right')
+        }
+    }
+}
 
 
 
 </script>
 
 <template>
-    <!-- <div id="map" class="map" style="width:600px; height: 400px"></div> -->
+    <div class="container mb-3">
+        <div id="map"></div>
+    </div>
+
 </template>
+
+<style lang="scss" scoped>
+#map {
+    width: 600px;
+    height: 400px;
+}
+</style>
