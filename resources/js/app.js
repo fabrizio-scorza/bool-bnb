2fdbb1f05bb0f79e4467a4957ac0d99f396582be
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-07-02 19:05:52 +0200

    ADD: client validation on the register view, to see if the password and the confirmation of it, are the same

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,3 +14,22 @@ const app = createApp({});
 app.component("map-component", MapComponent);
 app.component("address-component", AddressComponent);
 app.mount("#app");
+
+// Validazione della conferma password lato client al momento della registrazione dell'utente
+document.addEventListener("DOMContentLoaded", function() {
+
+    const form = document.getElementById("registrationForm");
+
+    if (form) {
+        form.addEventListener("submit", function(event) {
+            const password = document.getElementById("password").value;
+            const confirmPassword =document.getElementById("password-confirm").value;
+
+            if(password !== confirmPassword) {
+                alert("Le password non corrispondono.");
+                event.preventDefault();
+            }
+        });
+    }
+
+});
