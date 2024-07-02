8ba4c79587939437ee1a5e1d75aabd1aa4c9d5a4
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-07-02 12:21:54 +0200

    ADD: 'Piani' to the navbar

--- resources/views/admin/plans/index.blade.php
+++ resources/views/admin/plans/index.blade.php
@@ -4,19 +4,19 @@
 
 @section('content')
     <div class="container">
-        <h2 class="text-center">
+        <h2 class="text-center mt-2 mb-5">
             I nostri piani di sponsorizzazione
         </h2>
     </div>
     <div class="container">
-        <div class="row row-cols-3">
+        <div class="row d-flex justify-content-center">
             @foreach ($plans as $plan)
-            <div class="col">
+            <div class="col-3">
                 <div class="card mb-3">
-                    <div class="card-body">
+                    <div class="card-body text-center">
                         <h5 class="card-title">{{$plan->name}}</h5>
-                        <p class="card-text"><strong class="important_text">Prezzo: </strong>{{$plan->price}} €</p>
-                        <p class="card-text"><strong class="important_text">Durata: </strong>{{$plan->length}} ore</p>
+                        <p class="card-text my-1"><strong>Prezzo: </strong>{{$plan->price}} €</p>
+                        <p class="card-text"><strong>Durata: </strong>{{$plan->length}} ore</p>
                     </div>
                 </div>
             </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,6 +37,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{url('/') }}">{{ __('Home') }}</a>
                         </li>
+                        <li>
+                            <a class=" nav-link" href="{{route('admin.plans')}}">Piani</a>                    
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
