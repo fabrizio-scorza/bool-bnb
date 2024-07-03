4eb2dc0bbaeb4ea3121a9a6773f9754461658b8a
fabrizio-scorza <fabrizio.a.scorza@gmail.com>
2024-07-03 12:33:36 +0200

    Revert "Merge branch 'Vincenzo'"
    This reverts commit a6edb8ceff9968e1ee21270d89e9fd193511dc1c, reversing
    changes made to 2215cf4ba6ae3d852601e00e052ec725c1cbe59e.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}" id="registrationForm">
+                    <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="mb-4 row">
