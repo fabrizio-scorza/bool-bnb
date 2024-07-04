548117ef72168be8634e4fa52da5d8d27f4198fc
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-07-04 16:07:42 +0200

    ADD: carousel on the homepage

--- resources/js/Pages/Homepage.vue
+++ resources/js/Pages/Homepage.vue
@@ -15,7 +15,33 @@ export default {
 <template>
     <section class="carousel">
         <div class="container">
-            <h2>qui ci va il carosello</h2>
+            <div class="d-flex justify-content-center">
+                <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active" data-bs-interval="8000">
+                        <img src="https://www.produceshop.it/blog/wp-content/uploads/2021/09/MicrosoftTeams-image-54-930x620.jpg" class="d-block w-100 position-relative" alt="...">
+                        <!-- <h2 class="position-absolute top-50 start-0 text-white">Questo è il nostro sito!</h2> -->
+                        </div>
+                        <div class="carousel-item" data-bs-interval="8000">
+                        <img src="https://www.produceshop.it/blog/wp-content/uploads/2021/09/MicrosoftTeams-image-55-930x620.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item" data-bs-interval="8000">
+                        <img src="https://www.produceshop.it/blog/wp-content/uploads/2021/09/MicrosoftTeams-image-56-930x620.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item" data-bs-interval="8000">
+                        <img src="https://www.produceshop.it/blog/wp-content/uploads/2021/09/MicrosoftTeams-image-51-930x620.jpg" class="d-block w-100" alt="...">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
         </div>
     </section>
     <section class="searchbar">
@@ -68,7 +94,6 @@ export default {
                                         <a href="" class="me-3 link-underline link-underline-opacity-0">St</a>
                                         <a href="" class="link-underline link-underline-opacity-0">Sp</a>
                                     </div>
-
                                 </div>
                             </div>
                         </div>
@@ -83,5 +108,9 @@ export default {
 <style lang="scss" scoped>
 .hidden {
     display: none !important;
+}
+
+.slide{
+width: 800px;
 }
 </style>
